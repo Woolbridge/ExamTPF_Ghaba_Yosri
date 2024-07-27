e43fa0f226845b36165bd21ecbb6b9033f4e1f43
woolbridge <ghaba.yosri@gmail.com>
2024-07-27 12:08:09 +0100

    main v3

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
-<<<<<<< HEAD
-def somme(L) : 
-	s=0
-	for e in L :
-		s+=e
-	return s
-
-
 my_list=[1,5,7]
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-som=somme(my_list)
-print('la somme est =',som)
-=======
-my_list=[1,5,7]
-print("la somme est :",sum(my_list))
->>>>>>> feature_list
-=======
-=======
->>>>>>> feature_list
-=======
->>>>>>> feature_list
 if my_list :
 	print("la somme est :",sum(my_list))
 	print("le max est :",max(my_list))
 	print("le min est :",min(my_list))
 else :
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	print('liste vide')
->>>>>>> feature_list
-=======
+
 	print('liste vide')
->>>>>>> feature_list
-=======
+
 	print('liste vide')
->>>>>>> feature_list
+
